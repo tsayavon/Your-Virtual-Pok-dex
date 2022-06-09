74a671d7aedb194e6c4c585de327433e815dcc6e
tsayavon <sayavongtiffany05@gmail.com>
2022-06-08 23:19:50 -0400

    DOM objects declared

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+// DOM objects
+    //Grab name
+const pokeName = document.querySelector('.poke-name');
+    //Grab PokeID
+const pokeId = document.querySelector('.poke-id');
+    //Front Image
+const pokeFrontImage = document.querySelector('.poke-front-image');
+   //Back Image
+const pokeBackImage = document.querySelector('.poke-back-image');
+    //Type one
+const pokeTypeOne = document.querySelector('.poke-type-one');
+    //Type two
+const pokeTypeTwo = document.querySelector('.poke-type-two');
+    //Weight
+const pokeWeight = document.querySelector('.poke-weight');
+    //Height
+const pokeHeight = document.querySelector('.poke-height');
+
+
+
+
+
+console.log(pokeName);
+
+
 resultFromFetch = fetch('https://pokeapi.co/api/v2/pokemon/1')
     .then(res => res.json())
     .then(data => {
